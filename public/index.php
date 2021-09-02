6cda6b4c6a5cf745dccd7771ad265614871f4d05
ktomaszewski <tomaszewski.kamil.97@gmail.com>
2021-09-02 22:47:31 +0200

    Refactored project

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Kernel;
+declare(strict_types=1);
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+use Application\Kernel;
 
-return function (array $context) {
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+return static function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
