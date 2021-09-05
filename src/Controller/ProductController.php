b8de51b9355c74f5651d7d1164d16b65442ab30a
ktomaszewski <tomaszewski.kamil.97@gmail.com>
2021-09-05 18:30:06 +0200

    Refactor

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,53 +7,36 @@ namespace Application\Controller;
 use Application\Form\Product\ProductImportType;
 use Application\Service\Product\ProductImporter;
 use Application\Util\Flash;
-use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
-use Twig\Environment;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
 
 #[Route(path: '/product')]
-final class ProductController
+final class ProductController extends AbstractController
 {
-    public function __construct(
-        private RouterInterface $router,
-        private Environment $twigEnvironment,
-        private FormFactoryInterface $formFactory,
-        private RequestStack $requestStack,
-        private TranslatorInterface $translator,
-        private ProductImporter $productImporter
-    )
+    public function __construct(private TranslatorInterface $translator, private ProductImporter $productImporter)
     {
     }
 
     /**
-     * @throws RuntimeError
-     * @throws SyntaxError
-     * @throws LoaderError
      * @throws FileException
      */
     #[Route(path: '/import', name: 'app_product_import', methods: [Request::METHOD_GET, Request::METHOD_POST])]
     public function import(Request $request): Response
     {
-        $form = $this->formFactory->create(ProductImportType::class);
+        $form = $this->createForm(ProductImportType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->productImporter->handleUploadedFile($form->getData());
-            $this->requestStack->getSession()->getFlashBag()->add(Flash::SUCCESS, $this->translator->trans('import.success', [], 'product'));
+            $this->addFlash(Flash::SUCCESS, $this->translator->trans('import.success', [], 'product'));
 
-            return new RedirectResponse($this->router->generate('app_product_import'), Response::HTTP_FOUND);
+            return $this->redirectToRoute('app_product_import');
         }
 
-        return new Response($this->twigEnvironment->render('product/import.html.twig', ['form' => $form->createView()]));
+        return $this->render('product/import.html.twig', ['form' => $form->createView()]);
     }
 }
